790a39adbde4c74cad8d60c60236743d428c1e1f
Muhammad Aditya Ridharrahman <adityaridharrahman@gmail.com>
2024-09-13 08:01:17 +0700

    create simple test skeleton

---
+++ playwright.config.js
@@ -0,0 +1,30 @@
+// @ts-check
+const { defineConfig } = require('@playwright/test');
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// require('dotenv').config({ path: path.resolve(__dirname, '.env') });
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+module.exports = defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    // baseURL: 'http://127.0.0.1:3000',
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+  },
+});
---
+++ tests/test-structure.spec.js
@@ -0,0 +1,9 @@
+const { test } = require("playwright/test");
+
+test('Test case 1', async () => {
+    console.log("Test case 1")
+});
+
+test('Test case 2', async () => {
+    console.log("Test case 2")
+});
